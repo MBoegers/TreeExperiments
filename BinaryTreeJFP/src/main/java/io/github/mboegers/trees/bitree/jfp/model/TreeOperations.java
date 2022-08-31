e55d1ae3cb0502841bf63feda325bb2a8c6f9e7b
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2022-08-31 08:58:45 +0200

    fix insertion of subtree into another when deleting

--- BinaryTreeJFP/src/main/java/io/github/mboegers/trees/bitree/jfp/model/TreeOperations.java
+++ BinaryTreeJFP/src/main/java/io/github/mboegers/trees/bitree/jfp/model/TreeOperations.java
@@ -7,39 +7,39 @@ final class TreeOperations {
 
     static Tree delete(Tree tree, Integer value) {
         return switch (tree) {
-            case EmptyTree e -> e;
-            case SubTree(Tree l, Integer v, Tree r) when contains(l, value) -> delete(l, value);
-            case SubTree(Tree l, Integer v, Tree r) when contains(r, value) -> delete(r, value);
-            case SubTree(EmptyTree l, Integer v, EmptyTree r) when v.equals(value) -> new EmptyTree();
-            case SubTree(SubTree l, Integer v, EmptyTree r) when v.equals(value) -> l;
-            case SubTree(EmptyTree l, Integer v, SubTree r) when v.equals(value) -> r;
-            case SubTree(SubTree l, Integer v, SubTree r) when v.equals(value)-> insertSubTree(r, l);
+            case EmptyTree e, null -> e;
+            case SubTree(Tree l,Integer v,Tree r)when contains(l, value) -> delete(l, value);
+            case SubTree(Tree l,Integer v,Tree r)when contains(r, value) -> delete(r, value);
+            case SubTree(EmptyTree l,Integer v,EmptyTree r)when v.equals(value) -> new EmptyTree();
+            case SubTree(SubTree l,Integer v,EmptyTree r)when v.equals(value) -> l;
+            case SubTree(EmptyTree l,Integer v,SubTree r)when v.equals(value) -> r;
+            case SubTree(SubTree l,Integer v,SubTree r)when v.equals(value) -> insertSubTree(l, r);
             case SubTree subTree -> subTree;
         };
     }
 
     static Tree insertSubTree(Tree into, Tree subTree) {
         return switch (into) {
-            case EmptyTree e -> subTree;
-            case SubTree(Tree l, Integer v, SubTree r) -> new SubTree(l, v, insertSubTree(r, subTree));
-            case SubTree(Tree l, Integer v, EmptyTree r) -> new SubTree(l,v,subTree);
+            case EmptyTree e -> e;
+            case SubTree(Tree l,Integer v,SubTree r) -> new SubTree(l, v, insertSubTree(r, subTree));
+            case SubTree(Tree l,Integer v,EmptyTree r) -> new SubTree(l, v, subTree);
         };
     }
 
     static Tree insert(Tree tree, Integer value) {
         return switch (tree) {
             case EmptyTree e -> new SubTree(value);
-            case SubTree(Tree l, Integer v, Tree r) when v.equals(value) -> tree;
-            case SubTree(Tree l, Integer v, Tree r) when v > value -> new SubTree(insert(l, value), v, r);
-            case SubTree(Tree l, Integer v, Tree r) -> new SubTree(l, v, insert(r, value));
+            case SubTree(Tree l,Integer v,Tree r)when v.equals(value) -> tree;
+            case SubTree(Tree l,Integer v,Tree r)when(v > value) -> new SubTree(insert(l, value), v, r);
+            case SubTree(Tree l,Integer v,Tree r) -> new SubTree(l, v, insert(r, value));
         };
     }
 
     static boolean contains(Tree tree, Integer value) {
         return switch (tree) {
             case EmptyTree e -> false;
-            case SubTree(Tree l, Integer v, Tree r) when v.equals(value) -> true;
-            case SubTree(Tree l, Integer v, Tree r) -> contains(l,value) || contains(r,value);
+            case SubTree(Tree l,Integer v,Tree r)when v.equals(value) -> true;
+            case SubTree(Tree l,Integer v,Tree r) -> contains(l, value) || contains(r, value);
         };
     }
 }
